84f2e25ea2d080329ca7e8fef13c7c2f86da625d
mrcslance32 <mrcslance32@gmail.com>
2024-10-12 15:06:50 -0600

    update

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,7 +1,7 @@
 document.getElementById("burger").addEventListener("click", function () {
-    const menu = document.getElementById("menu");
-    const isActive = menu.classList.toggle("active");
-    this.innerHTML = isActive ? '✖' : '&#9776;';
+  const menu = document.getElementById("menu");
+  const isActive = menu.classList.toggle("active");
+  this.innerHTML = isActive ? '✖' : '&#9776;';
 });
 
 
@@ -10,14 +10,14 @@ const currentYear = new Date().getFullYear();
 
 const yearElement = document.getElementById('year');
 if (yearElement) {
-    yearElement.textContent = `${currentYear}`;
+  yearElement.textContent = `${currentYear}`;
 }
 
 const lastModifiedDate = document.lastModified;
 
 const modifiedElement = document.getElementById('modify');
 if (modifiedElement) {
-    modifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
+  modifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
 }
 // dates end
 
@@ -25,91 +25,91 @@ if (modifiedElement) {
 // New Stuff
 
 const temples = [
-    {
-      templeName: "Aba Nigeria",
-      location: "Aba, Nigeria",
-      dedicated: "2005, August, 7",
-      area: 11500,
-      imageUrl:
+  {
+    templeName: "Aba Nigeria",
+    location: "Aba, Nigeria",
+    dedicated: "2005, August, 7",
+    area: 11500,
+    imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
-    },
-    {
-      templeName: "Manti Utah",
-      location: "Manti, Utah, United States",
-      dedicated: "1888, May, 21",
-      area: 74792,
-      imageUrl:
+  },
+  {
+    templeName: "Manti Utah",
+    location: "Manti, Utah, United States",
+    dedicated: "1888, May, 21",
+    area: 74792,
+    imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
-    },
-    {
-      templeName: "Payson Utah",
-      location: "Payson, Utah, United States",
-      dedicated: "2015, June, 7",
-      area: 96630,
-      imageUrl:
+  },
+  {
+    templeName: "Payson Utah",
+    location: "Payson, Utah, United States",
+    dedicated: "2015, June, 7",
+    area: 96630,
+    imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
-    },
-    {
-      templeName: "Yigo Guam",
-      location: "Yigo, Guam",
-      dedicated: "2020, May, 2",
-      area: 6861,
-      imageUrl:
+  },
+  {
+    templeName: "Yigo Guam",
+    location: "Yigo, Guam",
+    dedicated: "2020, May, 2",
+    area: 6861,
+    imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
-    },
-    {
-      templeName: "Washington D.C.",
-      location: "Kensington, Maryland, United States",
-      dedicated: "1974, November, 19",
-      area: 156558,
-      imageUrl:
+  },
+  {
+    templeName: "Washington D.C.",
+    location: "Kensington, Maryland, United States",
+    dedicated: "1974, November, 19",
+    area: 156558,
+    imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
-    },
-    {
-      templeName: "Lima Perú",
-      location: "Lima, Perú",
-      dedicated: "1986, January, 10",
-      area: 9600,
-      imageUrl:
+  },
+  {
+    templeName: "Lima Perú",
+    location: "Lima, Perú",
+    dedicated: "1986, January, 10",
+    area: 9600,
+    imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
-    },
-    {
-      templeName: "Mexico City Mexico",
-      location: "Mexico City, Mexico",
-      dedicated: "1983, December, 2",
-      area: 116642,
-      imageUrl:
+  },
+  {
+    templeName: "Mexico City Mexico",
+    location: "Mexico City, Mexico",
+    dedicated: "1983, December, 2",
+    area: 116642,
+    imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
-    },
+  },
 
-    {
-      templeName: "Oakland Temple",
-      location: "Oakland, California, United States",
-      dedicated: "1964, November, 17",
-      area: 80157,
-      imageUrl:
+  {
+    templeName: "Oakland Temple",
+    location: "Oakland, California, United States",
+    dedicated: "1964, November, 17",
+    area: 80157,
+    imageUrl:
       "https://churchofjesuschristtemples.org/assets/img/temples/oakland-california-temple/oakland-california-temple-2654-main.jpg"
-    },
-    {
-      templeName: "Meridian Idaho Temple",
-      location: "Meridian, Idaho, United States",
-      dedicated: "2017, November, 19",
-      area: 67331,
-      imageUrl:
+  },
+  {
+    templeName: "Meridian Idaho Temple",
+    location: "Meridian, Idaho, United States",
+    dedicated: "2017, November, 19",
+    area: 67331,
+    imageUrl:
       "https://churchofjesuschristtemples.org/assets/img/temples/meridian-idaho-temple/meridian-idaho-temple-3302-main.jpg"
-    },
-    {
-      templeName: "Denver Colorado Temple",
-      location: "Centennial, Colorado, United States",
-      dedicated: "1986, November, 24",
-      area: 29117,
-      imageUrl:
+  },
+  {
+    templeName: "Denver Colorado Temple",
+    location: "Centennial, Colorado, United States",
+    dedicated: "1986, November, 24",
+    area: 29117,
+    imageUrl:
       "https://churchofjesuschristtemples.org/assets/img/temples/denver-colorado-temple/denver-colorado-temple-42455-main.jpg"
-    },
+  },
 
 
-    // Add more temple objects here...
-  ];
+  // Add more temple objects here...
+];
 
 //cards
 
@@ -117,10 +117,10 @@ const temples = [
 const templeContainer = document.getElementById('temple-container');
 
 temples.forEach(temple => {
-    const templeCard = document.createElement('div');
-    templeCard.className = 'temple-card';
+  const templeCard = document.createElement('div');
+  templeCard.className = 'temple-card';
 
-    templeCard.innerHTML = `
+  templeCard.innerHTML = `
         <h2>${temple.templeName}</h2>
         <p> Location: ${temple.location}</p>
         <p> Dedicated: ${temple.dedicated}</p>
@@ -128,10 +128,10 @@ temples.forEach(temple => {
         <img src="${temple.imageUrl}" alt="${temple.templeName}" loading="lazy">
     `;
 
-    templeContainer.appendChild(templeCard);
+  templeContainer.appendChild(templeCard);
 });
 
-// 
+//
 
 
 
