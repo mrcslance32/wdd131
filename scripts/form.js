90deb07c70b5026838506158c6670c9453c7d929
mrcslance32 <mrcslance32@gmail.com>
2024-10-19 19:18:44 -0600

    updates

--- scripts/form.js
+++ scripts/form.js
@@ -3,13 +3,55 @@ const currentYear = new Date().getFullYear();
 
 const yearElement = document.getElementById('year');
 if (yearElement) {
-  yearElement.textContent = `${currentYear}`;
+    yearElement.textContent = `${currentYear}`;
 }
 
 const lastModifiedDate = document.lastModified;
 
 const modifiedElement = document.getElementById('modify');
 if (modifiedElement) {
-  modifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
+    modifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
 }
 // dates end
+
+
+// form stuff
+
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+
+// Get the select element
+const productSelect = document.getElementById('product');
+
+// Populate the select options
+products.forEach(product => {
+    const option = document.createElement('option');
+    option.value = product.id; // Set the value to the product ID
+    option.textContent = product.name; // Set the display text to the product name
+    productSelect.appendChild(option); // Add the option to the select element
+});
