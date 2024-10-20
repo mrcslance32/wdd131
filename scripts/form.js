751c68b56d2221aa0305f0d27c1a0085bb4b9498
mrcslance32 <mrcslance32@gmail.com>
2024-10-19 18:05:29 -0600

    form

---
+++ scripts/form.js
@@ -0,0 +1,15 @@
+// dates
+const currentYear = new Date().getFullYear();
+
+const yearElement = document.getElementById('year');
+if (yearElement) {
+  yearElement.textContent = `${currentYear}`;
+}
+
+const lastModifiedDate = document.lastModified;
+
+const modifiedElement = document.getElementById('modify');
+if (modifiedElement) {
+  modifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
+}
+// dates end
